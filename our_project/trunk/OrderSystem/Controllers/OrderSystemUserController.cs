9271e6cfe9f28ac8231d9f6830269ccee7037ba9
galaxies1234@gmail.com <galaxies1234@gmail.com@8114fb8e-e7bd-a260-e41e-dbfe3e7cf6a2>
2011-11-02 16:59:22 +0000

    Будущий OrderSystemUserController,

---
+++ our_project/trunk/OrderSystem/Controllers/OrderSystemUserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderSystem.Controllers
+{
+    public class OrderSystemUserController : Controller
+    {
+        //
+        // GET: /OrderSystemUser/
+        //TODO Implement OrderSystemUserController
+        public ActionResult Index()
+        {
+            //TODO Get Users Count From EntityModel
+            ViewData["CountOfUsers"] = 1;
+            return View();
+        }
+
+    }
+}
