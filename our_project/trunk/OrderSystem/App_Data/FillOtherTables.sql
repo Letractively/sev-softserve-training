f6b10fab8323adbf83196d1b9afca3f21aa5bcc4
alxkop@gmail.com <alxkop@gmail.com@8114fb8e-e7bd-a260-e41e-dbfe3e7cf6a2>
2011-12-03 20:21:58 +0000

    Update DB Scripts

--- our_project/trunk/OrderSystem/App_Data/FillOtherTables.sql
+++ our_project/trunk/OrderSystem/App_Data/FillOtherTables.sql
@@ -182,240 +182,280 @@ INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('19" TFT Samsung E1920NR'
            ,'/5ìñ/50000:1/4:3/170°/160°/black/D-Sub'
            ,200
-           ,10)
+           ,10
+           ,'../../Content/Images/Items/SamsungE1920NR.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('19" TFT Asus VH197DR'
            ,'/5ìñ/10000000:1/170°/160°/wide/black/D-Sub'
            ,180
-           ,7)
+           ,7
+           ,'../../Content/Images/Items/AsusVH197DR.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('22" TFT LG E2241S'
            ,'/5ìñ/5000000 :1/176°/170°/wide/glossy black/D-Sub/LED'
            ,240
-           ,6)
+           ,6
+           ,'../../Content/Images/Items/LGE2241S.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('23" TFT LG M237WDP'
            ,'/5ìñ/20000:1/170°/160°/TV-tuner/D-Sub/DVI/HDMIx2'
            ,280
-           ,3)
+           ,3
+           ,'../../Content/Images/Items/LGM237WDP.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('Intel Pentium G6950'
            ,'/2.8G/3Mb/Tray LGA1156 Clarkdale'
            ,80
-           ,15)
+           ,15
+           ,'../../Content/Images/Items/PentiumG6950.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('Intel Core i5-2400'
            ,'/3.1G/DMI/6Mb/Box Sandy Bridge LGA1155'
            ,180
-           ,10)
+           ,10
+           ,'../../Content/Images/Items/IntelCorei5.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('Intel Celeron G530'
            ,'/2.4G/1024kb/DMI/Box Sandy Bridge LGA 1155, TDP 35 Watt'
            ,180
-           ,10)
+           ,10
+           ,'../../Content/Images/Items/IntelCeleron.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('Western Digital 500 Gb'
            ,'7200 Serial ATA II 16Mb WD5000AAKS'
            ,95
-           ,20)
+           ,20
+           ,'../../Content/Images/Items/WesternDigital500Gb.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('Western Digital 1000 Gb'
            ,'5400 Serial ATA III 32Mb WD10EADX'
            ,110
-           ,15)
+           ,15
+           ,'../../Content/Images/Items/WesternDigital1000Gb.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('Samsung 2000 Gb'
            ,'7200 Serial ATA II 32Mb  HD204U'
            ,140
-           ,8)
+           ,8
+           ,'../../Content/Images/Items/Samsung2000Gb.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('ATI Radeon HD 5450'
            ,'1024Mb DDR2/64-bit/DVI/VGA/HDMI/Sapphire'
            ,60
-           ,11)
+           ,11
+           ,'../../Content/Images/ATIRadeonHD5450.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('ATI Radeon HD 6770'
            ,'1024Mb DDR5/128-bit/HDMI/DVI/D-Sub/Asus'
            ,160
-           ,11)
+           ,11
+           ,'../../Content/Images/ATIRadeonHD6770.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('ATI Radeon HD 6870'
            ,'1024Mb DDR5//256-bit(920/4480Mhz)/2xDVI/HDMI/2xMiniDP/HiS'
            ,200
-           ,8)
+           ,8
+           ,'../../Content/Images/ATIRadeonHD6870.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('GeForce GTX560'
            ,'1024Mb/256bit(900/4212Mhz)/DDR5/2xDVI/miniHDMI/EVGA'
            ,180
-           ,6)
+           ,6
+           ,'../../Content/Images/Items/GeForceGTX560.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('CANON LBP-6300DN'
            ,'A4/30ppm/600dpi/USB2.0/10BASE-T/100BASE-TX'
            ,200
-           ,8)
+           ,8
+           ,'../../Content/Images/Items/CANONLBP-6300DN.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('Xerox Phaser 3140'
            ,'A4/1200x1200dpi/18ppm/8Mb/USB'
            ,85
-           ,8)
+           ,8
+           ,'../../Content/Images/Items/XeroxPhaser3140.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('Samsung ML-1661'
            ,'A4/1200x600dpi/16ppm/8Mb/USB2.0'
            ,80
-           ,8)
+           ,8
+           ,'../../Content/Images/SamsungML1661.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('HP DeskJet 2000'
            ,'À4/1200dpi/20ppm/USB2.0'
            ,60
-           ,8)
+           ,8
+           ,'../../Content/Images/HPDeskJet2000.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('Viewcon USB 2.0 HUB'
            ,'VE410/4 Ports/active/silver'
            ,30
-           ,10)
+           ,10
+           ,'../../Content/Images/Items/ViewconUSB2.0HUB.jpg')
 GO
 
 INSERT INTO [OrderSystem].[dbo].[Items]
            ([ItemName]
            ,[ItemDescriprion]
            ,[Price]
-           ,[Quantity])
+           ,[Quantity]
+           ,[Image])
      VALUES
            ('Genius NetScroll 110'
            ,'/black/optical/800dpi/3 but/ps/2'
            ,35
-           ,15)
+           ,15
+           ,'../../Content/Images/Items/GeniusNetScroll110Black.jpg')
 GO
 
 --- Fill 'Orders' table --- 
