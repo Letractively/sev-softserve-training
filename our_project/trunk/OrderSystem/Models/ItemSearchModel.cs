6342496fd2e676d34031d4e1ef8615fa249ecb43
alxkop <alxkop@gmail.com>
2011-12-25 08:15:52 +0000

    ItemSearch reload

---
+++ our_project/trunk/OrderSystem/Models/ItemSearchModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using OrderSystem.Models;
+
+namespace ItemSearcher.Models
+{
+    [Serializable]
+    public class ISModelState
+    {
+        public string sortmode;
+        public string SubstringFind;
+        public int SelectedItem;
+        public string Dimension;
+        public int order;
+        public int Quantity;
+        public Items item;
+        public Decimal Price;
+        public string[] ddl1items;
+        public string[] ddl2items;
+
+        public void OnInit()
+        {
+            this.sortmode = "name";
+            this.item = new Items();
+            this.SubstringFind = "";
+            this.ddl1items = new string[2] { "name", "description" };
+            this.ddl2items = new string [3] { "Item", "Box", "Package" };
+        }
+
+        public void ResetSelectedItem()
+        {
+            this.SelectedItem = -1;
+            this.Quantity = 1;
+            this.Price = 0.0M;
+        }
+
+        public void SetFilterOption(string mode, string find)
+        {
+            this.sortmode = mode;
+            this.SubstringFind = find;
+        }
+
+        public void CopyItem(Items tmp)
+        {
+            if (tmp.Image != null)
+            {
+                this.item.Image = tmp.Image;
+            }
+            else
+            {
+                this.item.Image = null;
+            }
+            this.item.ItemDescriprion = tmp.ItemDescriprion;
+            this.item.ItemID = tmp.ItemID;
+            this.item.ItemName = tmp.ItemName;
+            this.item.Price = tmp.Price;
+            this.item.Quantity = tmp.Quantity;
+        }
+
+        public ItemsOrder WriteToOrder()
+        {
+            ItemsOrder tmp = new ItemsOrder();
+            tmp.ItemID = this.item.ItemID;
+            tmp.ItemInfoID = this.item.ItemID;
+            tmp.OrderID = this.order;
+            tmp.Quantity = this.Quantity;
+            tmp.Dimension = this.Dimension;
+            return tmp;
+        }
+    }
+
+}
